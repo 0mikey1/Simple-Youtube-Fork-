15a374d077802059d8d1e154cb8e9980c0c7ae38
0mikey1 <mbent015@fiu.edu>
2022-06-05 21:23:44 -0400

    first commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+class SearchBar extends React.Component{
+  state = { term: '' };
+
+
+  onInputChange = event => { // assigned ass arrow because it is a callback that will be assigned to some child element
+    this.setState({ term: event.target.value });
+  };
+
+  onFormSubmit = event => { // assigned ass arrow because it is a callback that will be assigned to some child element
+    event.preventDefault();
+
+
+    this.props.onFormSubmit(this.state.term); 
+  };
+
+  render() {
+    return (
+      <div className='search-bar ui segment'>
+        <form onSubmit={this.onFormSubmit} className='ui form'>
+          <div className='field'>
+          <label>Video Search</label>
+          <input
+            type='text'
+            value={this.state.term}
+            onChange={this.onInputChange}   //is called anytime input is changed
+          />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
---
+++ src/components/VideoDetail.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const VideoDetail = ({ video }) => {
+if (!video) {
+    return (
+    <div class='ui segment'>
+      <p>Loading...</p>
+        <div class='ui active dimmer'>
+          <div class='ui loader'></div>
+        </div> 
+    </div>
+  );
+}
+
+const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+  return (
+    <div>
+      <div className ='ui embed'>
+        <iframe title='video player' src={videoSrc} />
+       </div>
+      <div className='ui segment'>
+        <h4 className='ui header'> {video.snippet.title}</h4>
+        <p>{video.snippet.description}</p>
+      </div>
+    </div>
+  );
+};
+
+
+export default VideoDetail;
